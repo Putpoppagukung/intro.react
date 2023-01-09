61e82216fb389f476f1fe52e1ce645368a29e96e
Pudding <Puttisan.put@gmail.com>
2023-01-09 16:20:54 +0700

    save

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import *as React from 'react'
+
+function getTitle(title){
+  return title;
+}
+
+function App() {
+  
+
+  return (
+    <div>
+      <div>
+        <h1>POPPAGU {getTitle('React')}</h1>
+
+        <label htmlFor="search">Search: </label>
+        <input type="text" id="search"/>
+      </div>
+    </div>
+  )
+}
+
+export default App
